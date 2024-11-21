2d1f35af34f25d8c71718f731ecb2d781ce7dcf7
Mason ma <MAMA14@oocl.com>
2024-11-21 11:21:49 +0800

    feat:complete ac1

--- src/afs/training/oo/Bus.java
+++ src/afs/training/oo/Bus.java
@@ -1,4 +1,17 @@
 package afs.training.oo;
 
 public class Bus {
+    private String name;
+
+    private int speed;
+
+    public Bus(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+
+    public void SpeedUp() {
+        speed += 5;
+        System.out.printf("%s:speed up to %d km/h", name, speed);
+    }
 }
